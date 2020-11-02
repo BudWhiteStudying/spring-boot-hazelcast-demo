aaaac3c31c678f6c3597a6daa2e36a48a2802a84
Tommaso Meledina <tommaso.meledina@it.ibm.com>
2020-11-03 00:28:38 +0100

    feat: specify cache key The POC is now more relevant, as it demonstrates caching based on a query parameter

--- src/main/java/com/budwhite/studying/hazelcast/demo/controller/MyController.java
+++ src/main/java/com/budwhite/studying/hazelcast/demo/controller/MyController.java
@@ -2,9 +2,12 @@ package com.budwhite.studying.hazelcast.demo.controller;
 
 import com.budwhite.studying.hazelcast.demo.service.MyService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class MyController {
@@ -13,12 +16,16 @@ public class MyController {
     private MyService myService;
 
     @GetMapping("resource")
-    public String getResource() throws InterruptedException {
-        return myService.waitThenSayHello();
+    public String getResource(@RequestParam String greetingRecipient) throws InterruptedException {
+        return myService.waitThenSayHello(greetingRecipient);
     }
 
     @DeleteMapping("cache")
     public boolean clearCache() {
-        return myService.clearResourceCache();
+        try {
+            return myService.clearResourceCache();
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+        }
     }
 }
--- src/main/java/com/budwhite/studying/hazelcast/demo/service/MyService.java
+++ src/main/java/com/budwhite/studying/hazelcast/demo/service/MyService.java
@@ -5,19 +5,24 @@ import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class MyService {
 
     @Autowired
     private CacheManager cacheManager;
 
-    @Cacheable("resource")
-    public String waitThenSayHello() throws InterruptedException {
+    @Cacheable(value = "resource", key = "#greetingRecipient")
+    public String waitThenSayHello(String greetingRecipient) throws InterruptedException {
         Thread.sleep(2000);
-        return "Hello";
+        return "Hello " + greetingRecipient;
     }
 
-    public boolean clearResourceCache() {
-        return cacheManager.getCache("resource").invalidate();
+    public boolean clearResourceCache() throws Exception {
+        return Optional.ofNullable(
+                cacheManager.getCache("resource"))
+                .orElseThrow(()->new Exception("'resource' cache not found"))
+                .invalidate();
     }
 }
